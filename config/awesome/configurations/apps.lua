71a044f2ffdbced9d940d3e49e094513df694a07
jadnw <jadenwu137@protonmail.com>
2022-12-30 12:54:33 +0700

    fix: use firefox as main browser

--- config/awesome/configurations/apps.lua
+++ config/awesome/configurations/apps.lua
@@ -14,9 +14,9 @@ return {
 		-- Default code editor
 		editor = "nvim",
 		-- Default web browser
-		browser = "chromium",
+		browser = "firefox-developer-edition",
 		-- Default dev browser
-		dev_browser = "firefox-developer-edition",
+		dev_browser = "chromium",
 		-- Default file manager
 		file_manager = "thunar",
 		-- Default network manager
